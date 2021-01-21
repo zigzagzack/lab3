74da2aef259087463ba8338195dd2e16829ce250
zigzagzack <ziggyzaggyzack@gmail.com>
2021-01-21 10:25:19 -0800

    did it

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -3,6 +3,7 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	projectClick();
 })
 
 /*
@@ -11,8 +12,26 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron p').toggleClass("active");
+		$('#testjs').text("do it again, I dare you");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$('a.thumbnail').click(projectClick);
+
+}
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "green");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+	   //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	   $(description).fadeOut();
+    }
 }
