1e3f1a09d02e38319ee63e5592e4cf141156cd9a
zigzagzack <ziggyzaggyzack@gmail.com>
2021-01-21 10:41:30 -0800

    did the nodeJS stuff

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,12 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+  if (nameToShow === undefined){
+    nameToShow = 'World';
+  }
+  console.log("Name is " + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
